ad7a567fe56282f78a481b93824029f5abbd2b41
Vikram Pal <vikramsonupal@gmail.com>
2020-10-05 12:04:07 -0700

    Worked through getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  //create array var to store doc elements with className
+  let results = [];
+  //create function to iterate each node
+  let search = (node) => {
+    //check current node to see if has classname
+    if (node.className.indexOf(className) !== -1) {
+      results.push(node);
+    }
+      //if it does push to array
+    //checkif current node has children
+    for (var i = 0; i < node.classList.length; i++) {
+      search(node.children[i]);
+    }
+      // if it does, recursively check if they have classname
+  };
+  //invoke function
+  search(document.body);
+  //return array
+  return results;
 };
