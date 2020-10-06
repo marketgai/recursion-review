01edd5be5b02cff3e2629f68eefdc6e38f32a42e
Vikram Pal <vikramsonupal@gmail.com>
2020-10-05 20:00:51 -0700

    Progress on stingifyJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,30 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  // '["one", "two"]',
+  // '{"a": "b", "c": "d"}'
+
+  if (json[0] === '[') {
+    let slicedArray = json.slice(1, json.length - 1);
+    let splitArray = slicedArray.split(', ');
+    let results = [];
+    splitArray.forEach(item => {
+      if (item[0] === '"') {
+        item.splice(0, 1);
+      }
+      if (item[item.length - 1] === '"') {
+        item.splice(0, -1);
+      }
+      console.log(item);
+    });
+    console.log(results);
+  }
+
+  if (json[0] === '{') {
+
+  }
+
+  if (json[0] === '"') {
+    return json.slice(1, json.length - 1);
+  }
 };
